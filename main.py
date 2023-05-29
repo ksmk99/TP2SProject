97dcedcf33b7df621bc178fdb7e5c6be7f7b49ad
Nagibator1488-SS <72550914+Nagibator1488-SS@users.noreply.github.com>
2023-05-29 21:14:13 +0500

    translate_text_to_en

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""Module for translate text"""
 from deep_translator import GoogleTranslator
+
+
+def translate_text_to_en(text):
+    """Function translate text to English"""
+    return GoogleTranslator(source='auto', target='en').translate(text)
 
 
 def translate_text_to_ru(text):
