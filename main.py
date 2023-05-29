f76f284707066adf80be58dd768aa4867a291a55
Nagibator1488-SS <72550914+Nagibator1488-SS@users.noreply.github.com>
2023-05-29 21:27:25 +0500

    generate_ru_text

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ def generate_en_text(text, length):
     return txt
 
 
+def generate_ru_text(text, length):
+    """Function generate Russian text"""
+    en_text = translate_text_to_en(text)
+    generated_en_text = generate_en_text(en_text, length)
+    generated_ru_text = translate_text_to_ru(generated_en_text)
+
+    return generated_ru_text
+
